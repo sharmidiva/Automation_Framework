579192838ed31099720d4f16ff1d3904420a2d51
vinayreddy3337 <97912978+vinayreddy3337@users.noreply.github.com>
2022-03-18 02:13:41 +0530

    Add files via upload

---
+++ datadrivenExcel/DDTExcel/src/main/java/Utilities/Constants.java
@@ -0,0 +1,9 @@
+package Utilities;
+
+public class Constants {
+
+	 public static final String URL = "https://demoqa.com/automation-practice-form";
+	    public static final String Path_TestData = "/DDTExcel/src/main/java/TestData";
+	    public static final String File_TestData = "Book1.xls";
+	
+}
